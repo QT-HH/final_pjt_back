da6d84f7c164d20520633b25842f177cc62cbb24
QT-HH <hyunheal@naver.com>
2020-11-24 03:08:54 +0900

    1124

--- communitys/communitys/serializers.py
+++ communitys/communitys/serializers.py
@@ -13,15 +13,15 @@ class ReviewListSerializer(serializers.ModelSerializer):
     
     class Meta:
         model= Review
-        fields= ('id', 'title', 'movie_title', 'user'),
+        fields= ('id', 'title', 'movie_title',),
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    comments = CommentSerializer(required=False, many=True)
+    # comments = CommentSerializer(required=False, many=True)
 
     class Meta:
         model = Review
-        fields = '__all__'
+        fields = ('title','movie_title','rank','content')
 
 
 
