6270c96df17ae5d956f13180cbb85745eedfc9f7
jieunkkk <vldzmf0805@naver.com>
2020-11-24 16:11:06 +0900

    change_url

--- communitys/communitys/urls.py
+++ communitys/communitys/urls.py
@@ -6,9 +6,7 @@ urlpatterns = [
     path('', views.review_list_create, name= 'review_list_create'),
     path('<int:review_pk>/detail/', views.review_detail, name='review_detail'),
     path('<int:review_pk>/', views.review_update_delete, name='review_update_delete'),
-    path('<int:review_pk>/comments/', views.create_comment, name='create_comment'),
-
-    path('comments/', views.comment_list),
+    path('<int:review_pk>/comments/', views.comment_list_create, name='comment_list_create'),
     path('comments/<int:comment_pk>/detail/', views.comment_detail),
     path('comments/<int:comment_pk>/', views.comment_update_delete),
 ]
