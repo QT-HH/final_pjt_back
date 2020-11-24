3a0add33bc39abf7ca76916822e19f1b64f7820e
jieunkkk <vldzmf0805@naver.com>
2020-11-24 19:57:52 +0900

    add_rank

--- communitys/movies/models.py
+++ communitys/movies/models.py
@@ -25,5 +25,6 @@ class Comment(models.Model):
     content = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    rank = models.IntegerField()
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='my_movie_comments')
