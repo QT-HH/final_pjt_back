8b992e9a4417f8210a2c14e98b4e945f07c23315
jieunkkk <vldzmf0805@naver.com>
2020-11-25 21:31:09 +0900

    add_fields

--- communitys/communitys/serializers.py
+++ communitys/communitys/serializers.py
@@ -16,7 +16,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model= Comment
-        fields= ('id','content','review','user')
+        fields= '__all__'
         read_only_fields = ('review','user')
 
 
